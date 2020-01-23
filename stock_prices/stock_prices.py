29921207066af1d840dc373b11cafd34fe2e7e5d
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-01-22 22:38:07 -0500

    working on stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,23 @@
 
 import argparse
 
+# find the maximum difference between the smallest and largetest prices
+# max profit, subtract a price by another price thats before it
+
 def find_max_profit(prices):
-  pass
+  if len(prices) <= 1:
+    return 0
+  
+  profit = 0
+  low = prices[0]
+  for i in range(1, len(prices)):
+    if prices[i] < low:
+      low = prices[i]
+    else:
+      profit = max(prices[i] - low, profit)
+      
+  return profit
+
 
 
 if __name__ == '__main__':
